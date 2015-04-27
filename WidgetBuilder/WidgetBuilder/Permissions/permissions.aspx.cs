77ced82b6f5e2808c9b59ade66e77dfeec3e5afd
Tyler Brown <robtbrown007@gmail.com>
2015-04-27 12:35:27 -0400

    Handle null datatype when looping through datatypes on member permissions.

--- WidgetBuilder/WidgetBuilder/Permissions/permissions.aspx.cs
+++ WidgetBuilder/WidgetBuilder/Permissions/permissions.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.Script.Serialization;
 
 using umbraco.BusinessLogic;
 using umbraco.cms.businesslogic.datatype;
+using Umbraco.Core.Logging;
 
 namespace WidgetBuilder
 {
@@ -20,139 +21,142 @@ namespace WidgetBuilder
 
         protected void Page_Load(object sender, EventArgs e)
         {
+           
             Guid propertyEditorGuid = new Guid("c324dcd5-4f2a-482a-94cd-1608a86328d6");
             WidgetBuilderUser user = new WidgetBuilderUser(Convert.ToInt32(userID));
-            //Log.Add(LogTypes.Debug, 0, "widget count=>"+ user.widgetPermissions.widgets.Count.ToString());
-
+            //Log.Add(LogTypes.Debug, 777, "widget count=>" + user.widgetPermissions.widgets.Count.ToString());
             HtmlGenericControl dataTypeDiv, hTag, configDiv, permissionTable, tr, th, td;
 
             //Response.Write()
+          
+                foreach (DataTypeDefinition thisDataType in DataTypeDefinition.GetAll().Where(d => d.DataType != null && d.DataType.Id == propertyEditorGuid))
+                {
 
-            foreach (DataTypeDefinition thisDataType in DataTypeDefinition.GetAll().Where(d => d.DataType.Id == propertyEditorGuid))
-            {
-               dataTypeDiv = new HtmlGenericControl("div");
-               mainWrapper.Controls.Add(dataTypeDiv);
-               dataTypeDiv.Attributes["class"] = "dataTypeDiv";
-               dataTypeDiv.Attributes["userID"] = userID;
-               dataTypeDiv.Attributes["guid"] = thisDataType.UniqueId.ToString();
-               
-               hTag = new HtmlGenericControl("h4");
-               dataTypeDiv.Controls.Add(hTag);
-               hTag.InnerHtml = "<a href='#'>" + thisDataType.Text+"</a>";
+                    dataTypeDiv = new HtmlGenericControl("div");
+                    mainWrapper.Controls.Add(dataTypeDiv);
+                    dataTypeDiv.Attributes["class"] = "dataTypeDiv";
+                    dataTypeDiv.Attributes["userID"] = userID;
+                    dataTypeDiv.Attributes["guid"] = thisDataType.UniqueId.ToString();
 
-               Widget_Builder_PrevalueEditor prevalueEditor = (Widget_Builder_PrevalueEditor)thisDataType.DataType.PrevalueEditor;
-               configDiv = new HtmlGenericControl("div");
-               configDiv.Attributes["class"] = "configDiv";
-               dataTypeDiv.Controls.Add(configDiv);
+                    hTag = new HtmlGenericControl("h4");
+                    dataTypeDiv.Controls.Add(hTag);
+                    hTag.InnerHtml = "<a href='#'>" + thisDataType.Text + "</a>";
 
-               //widget level
-               permissionTable = new HtmlGenericControl("table");
-               configDiv.Controls.Add(permissionTable);
+                    Widget_Builder_PrevalueEditor prevalueEditor = (Widget_Builder_PrevalueEditor)thisDataType.DataType.PrevalueEditor;
+                    configDiv = new HtmlGenericControl("div");
+                    configDiv.Attributes["class"] = "configDiv";
+                    dataTypeDiv.Controls.Add(configDiv);
 
-               tr = new HtmlGenericControl("tr");
-               permissionTable.Controls.Add(tr);
+                    //widget level
+                    permissionTable = new HtmlGenericControl("table");
+                    configDiv.Controls.Add(permissionTable);
 
-               td = new HtmlGenericControl("td");
-               tr.Controls.Add(td);
-               CheckBox hideWidgetForUser = new CheckBox();
-               hideWidgetForUser.Attributes["class"] = "hideWidgetForUser";
-               td.Controls.Add(hideWidgetForUser);
+                    tr = new HtmlGenericControl("tr");
+                    permissionTable.Controls.Add(tr);
 
-               try
-               {
-                   hideWidgetForUser.Checked = user.WidgetPermissions.widgets[thisDataType.UniqueId.ToString()].hide;
-               }
-               catch 
-               {
+                    td = new HtmlGenericControl("td");
+                    tr.Controls.Add(td);
+                    CheckBox hideWidgetForUser = new CheckBox();
+                    hideWidgetForUser.Attributes["class"] = "hideWidgetForUser";
+                    td.Controls.Add(hideWidgetForUser);
 
-               }
-    
-               th = new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml = "Hide All?";
+                    try
+                    {
+                        hideWidgetForUser.Checked = user.WidgetPermissions.widgets[thisDataType.UniqueId.ToString()].hide;
+                    }
+                    catch
+                    {
 
-               //element level
-               permissionTable = new HtmlGenericControl("table");
-               configDiv.Controls.Add(permissionTable);
+                    }
 
-               tr=new HtmlGenericControl("tr");
-               permissionTable.Controls.Add(tr);
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Hide All?";
 
-               th = new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml = "Hide?";
+                    //element level
+                    permissionTable = new HtmlGenericControl("table");
+                    configDiv.Controls.Add(permissionTable);
 
-               th=new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml="Title";
+                    tr = new HtmlGenericControl("tr");
+                    permissionTable.Controls.Add(tr);
 
-               th=new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml="Element";
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Hide?";
 
-               th=new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml="Type";
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Title";
 
-               th = new HtmlGenericControl("th");
-               tr.Controls.Add(th);
-               th.InnerHtml = "Description";
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Element";
 
-               foreach (WidgetElement thisElement in prevalueEditor.Configuration.elements){
-                   if (thisElement.prevalues != "")
-                   {
-                       try
-                       {
-                           //using textbox options generically since we are just needing the title/element names
-                           TextboxOptions options = jsonSerializer.Deserialize<TextboxOptions>(thisElement.prevalues);
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Type";
 
-                           tr=new HtmlGenericControl("tr");
-                           permissionTable.Controls.Add(tr);
+                    th = new HtmlGenericControl("th");
+                    tr.Controls.Add(th);
+                    th.InnerHtml = "Description";
 
-                           td = new HtmlGenericControl("td");
-                           tr.Controls.Add(td);
+                    foreach (WidgetElement thisElement in prevalueEditor.Configuration.elements)
+                    {
+                        if (thisElement.prevalues != "")
+                        {
+                            try
+                            {
+                                //using textbox options generically since we are just needing the title/element names
+                                TextboxOptions options = jsonSerializer.Deserialize<TextboxOptions>(thisElement.prevalues);
 
-                           CheckBox hidePropertyForUser = new CheckBox();
-                           hidePropertyForUser.Attributes["class"] = "hidePropertyForUser";
-                           td.Controls.Add(hidePropertyForUser);
-                           try
-                           {
-                               hidePropertyForUser.Checked = user.WidgetPermissions.widgets[thisDataType.UniqueId.ToString()].elements[options.elementName].hide;
-                           }
-                           catch (Exception e2)
-                           {}
-    
-                           td=new HtmlGenericControl("td");
-                           tr.Controls.Add(td);
-                           td.InnerHtml=HttpUtility.UrlDecode(options.title);
+                                tr = new HtmlGenericControl("tr");
+                                permissionTable.Controls.Add(tr);
 
-                           td = new HtmlGenericControl("td");
-                           tr.Controls.Add(td);
-                           td.InnerHtml = HttpUtility.UrlDecode(options.elementName);
+                                td = new HtmlGenericControl("td");
+                                tr.Controls.Add(td);
 
-                           td = new HtmlGenericControl("td");
-                           tr.Controls.Add(td);
-                           td.InnerHtml = HttpUtility.UrlDecode(thisElement.type);
+                                CheckBox hidePropertyForUser = new CheckBox();
+                                hidePropertyForUser.Attributes["class"] = "hidePropertyForUser";
+                                td.Controls.Add(hidePropertyForUser);
+                                try
+                                {
+                                    hidePropertyForUser.Checked = user.WidgetPermissions.widgets[thisDataType.UniqueId.ToString()].elements[options.elementName].hide;
+                                }
+                                catch (Exception e2)
+                                { }
 
-                           td = new HtmlGenericControl("td");
-                           tr.Controls.Add(td);
-                           td.InnerHtml = HttpUtility.UrlDecode(options.description);
-                       }
-                       catch (Exception e2)
-                       {
-                           Log.Add(LogTypes.Debug, 0, e2.Message);
-                       }
-                   }
-               }
+                                td = new HtmlGenericControl("td");
+                                tr.Controls.Add(td);
+                                td.InnerHtml = HttpUtility.UrlDecode(options.title);
 
-               tr = new HtmlGenericControl("tr");
-               permissionTable.Controls.Add(tr);
+                                td = new HtmlGenericControl("td");
+                                tr.Controls.Add(td);
+                                td.InnerHtml = HttpUtility.UrlDecode(options.elementName);
 
-               td = new HtmlGenericControl("td");
-               tr.Controls.Add(td);
-               td.Attributes["colspan"] = "4";
-               td.InnerHtml = "GUID: "+thisDataType.UniqueId;
-            }
+                                td = new HtmlGenericControl("td");
+                                tr.Controls.Add(td);
+                                td.InnerHtml = HttpUtility.UrlDecode(thisElement.type);
+
+                                td = new HtmlGenericControl("td");
+                                tr.Controls.Add(td);
+                                td.InnerHtml = HttpUtility.UrlDecode(options.description);
+                            }
+                            catch (Exception e2)
+                            {
+                                Log.Add(LogTypes.Debug, 0, e2.Message);
+                            }
+                        }
+                    }
+
+                    tr = new HtmlGenericControl("tr");
+                    permissionTable.Controls.Add(tr);
+
+                    td = new HtmlGenericControl("td");
+                    tr.Controls.Add(td);
+                    td.Attributes["colspan"] = "4";
+                    td.InnerHtml = "GUID: " + thisDataType.UniqueId;
+
+                }
         }
     }
 }
